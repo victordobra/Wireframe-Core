714ae90040bd2a3d8975bd5eff8abad3f8369192
Victor Dobra <victordobra327@gmail.com>
2022-06-26 13:13:53 +0300

    Deleted build files from repository

--- build/CMakeFiles/MG-Engine-Core.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libMG-Engine-Core.a"
-)
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Main/Programming/VS/MG-Engine-Core
-# Build directory: D:/Main/Programming/VS/MG-Engine-Core/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
