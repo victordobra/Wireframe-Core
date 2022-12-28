870a39c5fc7f1d3c5f313246278c1e46a247994e
victordobra <victordobra327@gmail.com>
2022-12-28 13:03:55 +0200

    include/Pair.hpp

--- include/Pair.hpp
+++ include/Pair.hpp
@@ -9,10 +9,14 @@ namespace wfe {
     /// @tparam T2 The type of the second value.
     template<class T1, class T2>
     struct pair {
+        /// @brief The first value's type.
         typedef T1 first_type;
+        /// @brief THe second value's type.
         typedef T2 second_type;
 
+        /// @brief The first value of the pair.
         first_type first{};
+        /// @brief The second value of the pair.
         second_type second{};
 
         /// @brief Construct a pair with both elements being at their default values.
