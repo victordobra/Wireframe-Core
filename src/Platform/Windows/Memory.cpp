1298dd73569073c8107e03241e44b89de194390f
MMG <73362859+victordobra@users.noreply.github.com>
2023-07-05 20:50:42 +0300

    Allowed internal thread regulation for Windows memory allocation

--- src/Platform/Windows/Memory.cpp
+++ src/Platform/Windows/Memory.cpp
@@ -8,19 +8,19 @@
 namespace wfe {
 	void* PlatformAllocateMemory(size_t size) {
 		// Allocate the memory using HeapAlloc
-		return (void*)HeapAlloc(GetProcessHeap(), HEAP_NO_SERIALIZE, size);
+		return (void*)HeapAlloc(GetProcessHeap(), 0, size);
 	}
 	void* PlatformAllocateZeroMemory(size_t nmemb, size_t size) {
 		// Allocate the memory using HeapAlloc and use the HEAP_ZERO_MEMORY flag
-		return (void*)HeapAlloc(GetProcessHeap(), HEAP_NO_SERIALIZE | HEAP_ZERO_MEMORY, nmemb * size);
+		return (void*)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, nmemb * size);
 	}
 	void* PlatformReallocateMemory(void* oldMem, size_t oldSize, size_t newSize) {
 		// Reallocate the memory using HeapReAlloc
-		return (void*)HeapReAlloc(GetProcessHeap(), HEAP_NO_SERIALIZE, oldMem, newSize);
+		return (void*)HeapReAlloc(GetProcessHeap(), 0, oldMem, newSize);
 	}
 	void PlatformFreeMemory(void* mem, size_t size) {
 		// Free the memory using HeapFree
-		auto result = HeapFree(GetProcessHeap(), HEAP_NO_SERIALIZE, mem);
+		auto result = HeapFree(GetProcessHeap(), 0, mem);
 	}
 }
 
