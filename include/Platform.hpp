390d1a4598b390f27d13d77229dfd7c76617a809
MMG <73362859+victordobra@users.noreply.github.com>
2023-08-14 18:48:55 +0300

    Updated Windows platform functions to include virtual address mapping

--- include/Platform.hpp
+++ include/Platform.hpp
@@ -3,20 +3,49 @@
 #include "Defines.hpp"
 
 namespace wfe {
-	/// @brief Allocates memory on the heap.
-	/// @param size The size, in bytes, of the allocated memory.
-	/// @return A pointer to the allocated memory.
-	void* PlatformAllocateMemory(size_t size);
-	/// @brief Allocates memory on the heap with all bytes set to 0.
-	/// @param size The size, in bytes, of the allocated memory.
-	/// @return A pointer to the allocated memory.
-	void* PlatformAllocateZeroMemory(size_t size);
-	/// @brief Reallocates the given heap memory, changing its size.
-	/// @param oldMem The memory to be reallocated.
-	/// @param newSize The new size, in bytes, of the reallocated memory.
-	/// @return A pointer to the new, reallocated memory.
-	void* PlatformReallocateMemory(void* oldMem, size_t newSize);
-	/// @brief Frees the given heap memory.
-	/// @param mem The memory to be freed.
-	void PlatformFreeMemory(void* mem);
+	struct PerformanceInfo {
+		/// @brief The size of a memory page, in bytes.
+		size_t pageSize;
+		/// @brief The total ammount of physical memory available, in bytes.
+		uint64_t totalPhysicalMemorySize;
+		/// @brief The available ammount of physical memory available, in bytes.
+		uint64_t availablePhysicalMemorySize;
+		/// @brief The total user virtual memory space size, in bytes.
+		size_t totalVirtualMemorySize;
+		/// @brief The available user virtual memory space size, in bytes.
+		size_t availableVirtualMemorySize;
+		/// @brief The lowest memory address accessible to applications.
+		void* minAppAddress;
+		/// @brief The highest memory address accessible to applications.
+		void* maxAppAddress;
+
+		/// @brief The current number of processes.
+		size_t processCount;
+		/// @brief The current number of threads.
+		size_t threadCount;
+		/// @brief The number of available logical processors.
+		size_t processorCount;
+	};
+
+	/// @brief Gets the system's performance info.
+	/// @param performanceInfo A reference to the variable in which the performance info will be written.
+	void GetPerformanceInformation(PerformanceInfo& performanceInfo);
+
+	/// @brief Reserves a region of pages in the virtual address space of the current process.
+	/// @param address The starting address to reserve in the address space. Can be set to nullptr for the system to choose the starting address.
+	/// @param size The size of the region to be reserved, in bytes.
+	/// @return A pointer to the base address of the allocated region, or nullptr if the operation failed.
+	void* ReserveVirtualAddressSpace(void* address, size_t size);
+	/// @brief Frees a region of pages from the virtual address space of the current process.
+	/// @param address The starting address of the region to free.
+	void FreeVirtualAddressSpace(void* address);
+	/// @brief Commits a region of pages from the virtual address space of the current process by allocating required memory charges.
+	/// @param address The starting address of the region to commit.
+	/// @param size The size of the region to commit, in bytes.
+	/// @return True if the given region was commited successfully, otherwise false.
+	bool8_t CommitVirtualAddressSpace(void* address, size_t size);
+	/// @brief Uncommits a region of pages from the virtual address space of the current process by freeing all allocated memory charges.
+	/// @param address The starting address of the region to uncommit.
+	/// @param size The size of the reguin to uncommit, in bytes.
+	void DecommitVirtualAddressSpace(void* address, size_t size);
 }
--- src/Platform/Windows/Memory.cpp
+++ src/Platform/Windows/Memory.cpp
@@ -3,24 +3,52 @@
 #ifdef WFE_PLATFORM_WINDOWS
 
 #include "Platform.hpp"
-#include <heapapi.h>
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <psapi.h>
 
 namespace wfe {
-	void* PlatformAllocateMemory(size_t size) {
-		// Allocate the memory using HeapAlloc
-		return (void*)HeapAlloc(GetProcessHeap(), 0, size);
+	void GetPerformanceInformation(PerformanceInfo& performanceInfo) {
+		// Get all structs required for constructing the performance info
+		SYSTEM_INFO systemInfo;
+		PERFORMANCE_INFORMATION perfInfo;
+		MEMORYSTATUSEX memoryStatus;
+		memoryStatus.dwLength = sizeof(MEMORYSTATUSEX);
+
+		GetSystemInfo(&systemInfo);
+		::GetPerformanceInfo(&perfInfo, sizeof(PERFORMANCE_INFORMATION));
+		GlobalMemoryStatusEx(&memoryStatus);
+
+		// Set the performance info based on the retrieved structs
+		performanceInfo.pageSize = (size_t)perfInfo.PageSize;
+		performanceInfo.totalPhysicalMemorySize = (uint64_t)memoryStatus.ullTotalPhys;
+		performanceInfo.availablePhysicalMemorySize = (uint64_t)memoryStatus.ullAvailPhys;
+		performanceInfo.totalVirtualMemorySize = (uint64_t)memoryStatus.ullTotalVirtual;
+		performanceInfo.availableVirtualMemorySize = (uint64_t)memoryStatus.ullAvailVirtual;
+		performanceInfo.minAppAddress = systemInfo.lpMinimumApplicationAddress;
+		performanceInfo.maxAppAddress = systemInfo.lpMaximumApplicationAddress;
+		
+		performanceInfo.processCount = (size_t)perfInfo.ProcessCount;
+		performanceInfo.threadCount = (size_t)perfInfo.ThreadCount;
+		performanceInfo.processorCount = (size_t)systemInfo.dwNumberOfProcessors;
 	}
-	void* PlatformAllocateZeroMemory(size_t size) {
-		// Allocate the memory using HeapAlloc and use the HEAP_ZERO_MEMORY flag
-		return (void*)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, size);
+
+	void* ReserveVirtualAddressSpace(void* address, size_t size) {
+		// Reserve the wanted region using VirtualAlloc with the MEM_RESERVE flag set
+		return VirtualAlloc(address, size, MEM_RESERVE, PAGE_READWRITE);
 	}
-	void* PlatformReallocateMemory(void* oldMem, size_t newSize) {
-		// Reallocate the memory using HeapReAlloc
-		return (void*)HeapReAlloc(GetProcessHeap(), 0, oldMem, newSize);
+	void FreeVirtualAddressSpace(void* address) {
+		// Free the given region using VirtualFree with the MEM_RELEASE flag set
+		VirtualFree(address, 0, MEM_RELEASE);
 	}
-	void PlatformFreeMemory(void* mem) {
-		// Free the memory using HeapFree
-		auto result = HeapFree(GetProcessHeap(), 0, mem);
+	bool8_t CommitVirtualAddressSpace(void* address, size_t size) {
+		// Commit the given region using VirtualAlloc with the MEM_COMMIT flag set
+		return VirtualAlloc(address, size, MEM_COMMIT, PAGE_READWRITE);
+	}
+	void DecommitVirtualAddressSpace(void* address, size_t size) {
+		// Uncommit the given region using VirtualFree with the MEM_DECOMMIT flag set
+		VirtualFree(address, size, MEM_DECOMMIT);
 	}
 }
 
