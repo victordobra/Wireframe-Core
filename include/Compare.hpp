c1223d8bbc1ca57578f16a50fd0926b088a97c84
MMG <73362859+victordobra@users.noreply.github.com>
2023-09-26 20:40:23 +0300

    Added string template specializations for comparison function structs

--- include/Compare.hpp
+++ include/Compare.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Defines.hpp"
+#include "Memory.hpp"
 
 namespace wfe {
     /// @brief Function struct for performing comparisons.
@@ -41,4 +42,29 @@ namespace wfe {
             return val1 > val2;
         }
     };
+
+    /// @brief Checks if the first string is lower than the other.
+    /// @param str1 The first string in the comparison.
+    /// @param str2 The second string in the comparison.
+    /// @return True if the first string is lower than the second, otherwise false.
+    template<>
+    WFE_INLINE bool8_t LowerComp<const char_t*>::operator()(const char_t* const& str1, const char_t* const& str2) const {
+        return strcmp(str1, str2) < 0;
+    }
+    /// @brief Checks if the two given strings are equal.
+    /// @param str1 The first string in the comparison.
+    /// @param str2 The second string in the comparison.
+    /// @return True if the two strings are equal, otherwise false.
+    template<>
+    WFE_INLINE bool8_t EqualComp<const char_t*>::operator()(const char_t* const& str1, const char_t* const& str2) const {
+        return !strcmp(str1, str2);
+    }
+    /// @brief Checks if the first string is higher than the other.
+    /// @param str1 The first string in the comparison.
+    /// @param str2 The second string in the comparison.
+    /// @return True if the first string is higher than the second, otherwise false.
+    template<>
+    WFE_INLINE bool8_t HigherComp<const char_t*>::operator()(const char_t* const& str1, const char_t* const& str2) const {
+        return strcmp(str1, str2) > 0;
+    }
 }
